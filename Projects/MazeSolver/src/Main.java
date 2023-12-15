b2b8dd80459e8f010b457f18e9c85c81e4bc551c
aidenpratt <u1458057@umail.utah.edu>
2023-12-14 21:05:35 -0700

    updates

---
+++ Projects/MazeSolver/src/Main.java
@@ -0,0 +1,9 @@
+import assingment08.PathFinder;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ Projects/MazeSolver/src/assingment08/TestPathFinder.java
@@ -0,0 +1,26 @@
+package assingment08;
+
+
+
+public class TestPathFinder {
+
+  public static void main(String[] args) {
+
+    /*
+     * The below code assumes you have a file "tinyMaze.txt" in your project folder.
+     * If PathFinder.solveMaze is implemented, it will create a file "tinyMazeOutput.txt" in your project folder.
+     * 
+     * REMEMBER - You have to refresh your project to see the output file in your package explorer. 
+     * You are still required to make JUnit tests...just lookin' at text files ain't gonna fly. 
+     */
+    PathFinder.solveMaze("/Users/aidenpratt/Documents/AP-6012/Week4/Day5/Assingment08/mazes/unsolvable.txt",
+            "OUTPUT1.txt");
+
+    PathFinder.solveMaze("/Users/aidenpratt/Documents/AP-6012/Week4/Day5/Assingment08/mazes/tinyMaze.txt",
+            "OUTPUT2.txt");
+
+    PathFinder.solveMaze("/Users/aidenpratt/Documents/AP-6012/Week4/Day5/Assingment08/mazes/bigMaze.txt",
+            "OUTPUT3.txt");
+  }
+}
+
